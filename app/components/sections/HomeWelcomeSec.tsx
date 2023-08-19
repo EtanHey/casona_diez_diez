f1ac90b1651333034782937fc6e63b472131aafa
Etan Joseph Heyman <etan@heyman.net>
2023-08-19 13:53:53 -0500

    Fix some responsiveness

--- app/components/sections/HomeWelcomeSec.tsx
+++ app/components/sections/HomeWelcomeSec.tsx
@@ -5,15 +5,15 @@ import Image from "next/image";
 const HomeWelcomeSec = () => {
   return (
     <div className="h-screen w-full">
-      <div className="relative h-[303.552px] w-[458.30400000000003px] sm:h-[40%] sm:w-full left-1/2 -translate-x-1/2">
+      <div className="relative h-fit w-fit left-1/2 -translate-x-1/2">
         <Image
         className=""
           src={"/hero_picture.jpg"}
           alt="Loby picture"
           height={303.552}
-           width={458.30400000000003} 
+          width={458.30400000000003} 
         />
-        <div className="tint w-full h-full bg-[#721800] opacity-30 absolute top-0 z-10"></div>
+        <div className="tint w-full h-full bg-[#721800] opacity-30 fixed top-0 z-10"></div>
         <CddDarkLogo className="absolute top-1/4 left-1/2 -translate-x-1/2 -translate-y-1/2 z-20" />
       </div>
     </div>
