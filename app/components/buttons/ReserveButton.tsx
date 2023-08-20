4bd68b723036a407d76e75e6752055aa8b3961a6
Etan Joseph Heyman <etan@heyman.net>
2023-08-20 11:48:03 -0500

    Add upperletter and insets for ReserveButton

--- app/components/buttons/ReserveButton.tsx
+++ app/components/buttons/ReserveButton.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const ReserveButton = () => {
   return (
-    <button className="bg-cdd-red absolute left-1/2 top-1/2 -translate-x-1/2 rounded-full px-6 py-3 font-semibold text-white">
-      Reservas
+    <button className="bg-cdd-red top-21/40 absolute left-1/2 -translate-x-1/2 rounded-full px-4 py-[10px] font-medium uppercase text-white">
+      reservas
     </button>
   );
 };
