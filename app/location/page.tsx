c88f421a0d61247d1f9e79ea0c4019b6096ec2ed
Etan Joseph Heyman <etan@heyman.net>
2023-08-31 10:17:35 -0500

    Location page setup start

--- app/location/page.tsx
+++ app/location/page.tsx
@@ -1,12 +1,23 @@
-import Header from '@/app/components/Header'
-import React from 'react'
+import Header from "@/app/components/Header";
+import React from "react";
+import HeartSVG from "../components/svgs/HeartSVG";
 
 const Location = () => {
   return (
-    <><Header>
-      UBICACIÓN
-      </Header></>
-  )
-}
+    <>
+      <Header>UBICACIÓN</Header>
 
-export default Location
+      <div className="flex w-full flex-col place-content-center items-center gap-8">
+        <div className="flex w-full place-content-center items-center gap-2">
+          <HeartSVG />
+          <p className="text-sm font-medium uppercase">en el santelmo</p>
+        </div>
+        <div className="flex flex-col gap-4">
+          <p className="font-semibold text-base">Piedras 1010 e/ Carlos Calvo</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Location;
