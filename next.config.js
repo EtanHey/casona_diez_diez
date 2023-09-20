9d86bc56c372ffe08acdcf73cd8002ce3294c3a2
Etan Joseph Heyman <etan@heyman.net>
2023-09-19 21:03:22 -0500

    another one

--- next.config.js
+++ next.config.js
@@ -4,12 +4,6 @@ const nextConfig = {
     serverComponentsExternalPackages: ["mongodb"],
   },
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "uploadthing-prod.s3.us-west-2.amazonaws.com",
-      },
-    ],
     domains: [
       "uploadthing-prod.s3.us-west-2.amazonaws.com",
       "utfs.io",
