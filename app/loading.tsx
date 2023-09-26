03c9be2adb925ebe08a8d39b3c845e3602d7f06e
Etan Joseph Heyman <etan@heyman.net>
2023-09-26 14:08:24 -0500

    change loader opacity

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,3 +1,3 @@
 export default function Loading() {
-  return <div className="fixed top-0 bg-cdd-red/70 flex h-screen w-full justify-center z-[60] items-center scroll-smooth"><h1 className="bg-white px-5 py-2 rounded-full">Loading...</h1></div>;
+  return <div className="fixed top-0 bg-cdd-red/40 flex h-screen w-full justify-center z-[60] items-center scroll-smooth"><h1 className="bg-white px-5 py-2 rounded-full">Loading...</h1></div>;
 }
