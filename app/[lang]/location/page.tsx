09e1f7b754df2ade4788d698ae7e7dce475381d7
Etan Joseph Heyman <etan@heyman.net>
2023-12-07 10:46:40 -0600

    Location page half translated

--- app/[lang]/location/page.tsx
+++ app/[lang]/location/page.tsx
@@ -3,32 +3,41 @@ import Header from "@/app/[lang]/components/Header";
 import MapComponent from "@/app/[lang]/components/MapComponent";
 import HeartSVG from "@/app/[lang]/components/svgs/HeartSVG";
 import React from "react";
+import { getDictionary } from "../dictionaries";
 
-const Location = () => {
+const Location = async ({ params: { lang } }: { params: { lang: string } }) => {
+  const dict = await getDictionary(lang);
+
+  const locationDictionary = dict.location;
   return (
     <>
-      <Header>UBICACIÓN</Header>
+      <Header>{locationDictionary.mainText}</Header>
       <div className="mb-4 flex w-full flex-col place-content-center items-center">
         <div className="mb-8 flex w-full place-content-center items-center gap-2">
           <HeartSVG />
-          <p className="text-sm font-medium uppercase">en el santelmo</p>
+          <p className="text-sm font-medium uppercase">
+            {locationDictionary.subText}
+          </p>
         </div>
         <div className="flex w-33.5/40 flex-col text-center">
           <p className="text-base font-semibold">
-            Piedras 1010 e/ Carlos Calvo
+            Piedras 1010 {locationDictionary.onStreet} Carlos Calvo
           </p>
         </div>
         <ColoredBreak small bg="bg-cdd-red" />
         <p className="flex w-33.5/40 flex-col text-center text-sm">
-          A tres cuadras de la feria de los domingos
+          {locationDictionary.threeBlocksFrom}
+          {locationDictionary.sundayFair}
         </p>
         <ColoredBreak small bg="bg-cdd-red" />
         <p className="flex w-33.5/40 flex-col px-6 text-center text-sm">
-          A dos cuadras de la estación de subte de la linea C
+          {locationDictionary.twoBlocksFrom}
+          {locationDictionary.lineC}
         </p>
         <ColoredBreak small bg="bg-cdd-red" />
         <p className="flex w-33.5/40 flex-col px-6 text-center text-sm">
-          A tres cuadras de la avenida 9 de julio
+          {locationDictionary.threeBlocksFrom}
+          {locationDictionary.nineOfJulyAvenue}
         </p>
         <ColoredBreak bg="bg-cdd-red" />
         <div className="flex gap-3 px-4">
